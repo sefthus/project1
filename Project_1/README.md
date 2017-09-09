883405fc14422ba7c0a6f610ae1b5d8459889708
sefthus <elisabeth.strom95@gmail.com>
2017-09-09 16:35:08 +0200

    Added directory to gitignore, changed code in python file
    The final code for plotting in python is added.
    Build directories and txt files added to .gitignore
    Removed readme.md file that shouldn't be here, it belonged to another directory

--- Project_1/README.md
+++
@@ -1,4 +0,0 @@
-# FYS4150
-Computational physics course, 1. semester of master thesis. 
-small change, another small change.
-More change.
--- Project_1/plotting_1b.py
+++ Project_1/plotting_1b.py
@@ -2,28 +2,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-N = np.genfromtxt('project1c_n_10.txt',skip_header=2) 	 # read file, ignore first two lines
-x = N[0:,0]						 # x from file
-u = N[0:,1] 					 # analytical result, extract from file
-v = N[0:,2]				 # numerical result, extract from file
+N1 = np.genfromtxt('project1b_n_10.txt',skip_header=2) 	 # read file, ignore first two lines
+x1 = N1[0:,0]						 # x from file
+u1 = N1[0:,1] 					 # analytical result, extract from file
+v1 = N1[0:,2]				 # numerical result, extract from file
 
-def plot_formatting(fam='serif',fam_font='Computer Modern Roman',font_size=16,tick_size=16):
-	""" you get to define what font and size of xlabels and axis ticks you"""
-	"""like, if you want bold text or not.								  """
+N2 = np.genfromtxt('project1b_n_100.txt',skip_header=2) 	 # read file, ignore first two lines
+x2 = N2[0:,0]						 # x from file
+u2 = N2[0:,1] 					 # analytical result, extract from file
+v2 = N2[0:,2]				 # numerical result, extract from file
 
-	plt.rc('text',usetex=True)
-	axis_font={'family': fam,'serif':[fam_font],'size':font_size}
+N3 = np.genfromtxt('project1b_n_1000.txt',skip_header=2) 	 # read file, ignore first two lines
+x3 = N3[0:,0]						 # x from file
+u3 = N3[0:,1] 					 # analytical result, extract from file
+v3 = N3[0:,2]				 # numerical result, extract from file
+
+def plot_formatting():
+	"""pretty formatting of plots """
+	plt.rc('text',usetex=True) # only works with matplotlib version below 1.5.2, comment out elsewise
+	axis_font={'family': 'serif','serif':'Computer Modern Roman','size':16}
 	plt.rc('font',**axis_font)
 	plt.rc('font',weight ='bold')
-	plt.xticks(fontsize=tick_size)
-	plt.yticks(fontsize=tick_size)
-#plot_formatting()
+	plt.xticks(fontsize=16)
+	plt.yticks(fontsize=16)
+
+plot_formatting()
 #plt.rc('text', usetex=True)
 #plt.rc('font', family='serif')
-plt.plot(x,u)
-plt.plot(x,v)
-plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.legend(['analytical','numerical'])
+
+plt.subplot(3,1,1)
+plt.plot(x1,u1,x1,v1,linewidth=2.0)
+plt.legend(['Analytical','Numerical'],loc ='upper right')
+plt.setp(plt.gca().get_legend().get_texts(), fontsize=16)
+plt.title('n=10',fontsize=16)
+plt.xlabel('$x$')
+plt.ylabel('$u(x)$')
+
+plt.subplot(3,1,2)
+plt.plot(x2,u2,x2,v2,linewidth=2.0)
+plt.legend(['Analytical','Numerical'],loc ='upper right')
+plt.setp(plt.gca().get_legend().get_texts(), fontsize=16)
+plt.title('n=100',fontsize=16)
+plt.xlabel('$x$')
+plt.ylabel('$u(x)$')
+
+plt.subplot(3,1,3)
+plt.plot(x3,u3,x3,v3,linewidth=2.0)
+plt.legend(['Analytical','Numerical'],loc ='upper right')
+plt.setp(plt.gca().get_legend().get_texts(), fontsize=16)
+plt.title('n=1000',fontsize=16)
+plt.xlabel('$x$')
+plt.ylabel('$u(x)$')
+
+
+plt.suptitle('Exact and numerical solution of set of linear equations',fontsize=16)
+plt.tight_layout()
+plt.subplots_adjust(top=0.88)
 plt.show()
 	
